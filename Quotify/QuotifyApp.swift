6f3f76842b296764ffa39af0b6dcabf3a3f247ef
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2022-11-28 17:54:58 +0300

    Initial Commit

---
+++ Quotify/QuotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotifyApp.swift
+//  Quotify
+//
+//  Created by Ardi Jorganxhi on 28.11.22.
+//
+
+import SwiftUI
+
+@main
+struct QuotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
